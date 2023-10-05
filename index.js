7ec581ab3fbf33fac0e87b569179ddda926c1fcd
Neha <nehad2000@gmail.com>
2023-10-05 13:00:17 -0500

    block07 finished workshop

---
+++ index.js
@@ -0,0 +1,18 @@
+//Pseudocode: Create a string that states "You have received this message because you have been chosen to open an important vault."//
+//Pseudocode: Create a string that states "Here is the secret combination://
+// Pseudocode: Assign variable 1 as 10 using a unique arithmetic operator.
+//Pseudocode: Assign variable 2 as 40 using a unique arithmetic operator.
+//Pseudocode: Assign variable 3 as 39 using a unique arithmetic operator.
+//Pseudocode: Create a dialog box to reveal the code
+let str1 = "You hace received this message because you have been chosen to open a vault.";
+let str2 = "Here is the secret combination: "
+console.log(str1 +" " + str2)
+//The 2 strings above will be concacated to show the message typed in the pseudocode.
+
+
+const num1 = 3 + 7 //added 3+7 +10
+const num2 = 15 * 3 //multiplied 15*3+40
+const num3 = 64 - 25 //subtracted 64-25=39
+
+console.log(num1 + "-" + num2 + "-" +num3) //show in dialog box
+alert(str1 + " " + str2 + num1 + "-" + num2 + "-" +num3)
